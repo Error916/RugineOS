ffc73515f90d88fe996263541599c928cd96b53c
Error916 <ettorenigma@gmail.com>
2023-02-06 18:35:50 +0100

    Implement Tests and refactor the code

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -4,29 +4,22 @@
 #![test_runner(error_os_rust::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-use core::panic::PanicInfo;
 use error_os_rust::println;
+use core::panic::PanicInfo;
 
-#[no_mangle]
+#[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
-
-    #[cfg(test)]
     test_main();
 
     loop {}
 }
 
-/// This function is called on panic.
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
-}
-
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     error_os_rust::test_panic_handler(info)
+}
+
+#[test_case]
+fn test_println() {
+    println!("test_println output");
 }
