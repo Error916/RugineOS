3fe8d72844b6b37a032f32d3e63fa5b2435cde5c
Error916 <ettorenigma@gmail.com>
2023-02-07 11:21:59 +0100

    heap implementation with a default allocator, next commit we will create one from scratch

--- src/lib.rs
+++ src/lib.rs
@@ -2,15 +2,19 @@
 #![cfg_attr(test, no_main)]
 #![feature(custom_test_frameworks)]
 #![feature(abi_x86_interrupt)]
+#![feature(alloc_error_handler)]
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
+pub mod allocator;
 pub mod gdt;
 pub mod interrupts;
 pub mod memory;
 pub mod serial;
 pub mod vga_buffer;
+
+extern crate alloc;
 
 pub fn hlt_loop() -> ! {
     loop {
@@ -39,6 +43,11 @@ pub fn init() {
     interrupts::init_idt();
     unsafe { interrupts::PICS.lock().initialize() };
     x86_64::instructions::interrupts::enable();
+}
+
+#[alloc_error_handler]
+fn alloc_error_handler(layout: alloc::alloc::Layout) -> ! {
+    panic!("allocation error: {:?}", layout)
 }
 
 /*************/
